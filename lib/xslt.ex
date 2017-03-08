05fec9df25498f0908d4b6593fb0a757f0c4b352
John Hamelink <john@johnhamelink.com>
2017-03-08 11:53:39 +0000

    Ensure the use of the Porcelain.Result struct in responses

--- lib/xslt.ex
+++ lib/xslt.ex
@@ -10,40 +10,40 @@ defmodule Xslt do
     |> handle_output
   end
 
-  defp handle_output(%{status: 0, out: output}),
+  defp handle_output(%Result{status: 0, out: output}),
   do: {:ok, output}
 
-  defp handle_output(%{status: 1}),
+  defp handle_output(%Result{status: 1}),
   do: {:error, "No argument"}
 
-  defp handle_output(%{status: 2}),
+  defp handle_output(%Result{status: 2}),
   do: {:error, "Too many parameters"}
 
-  defp handle_output(%{status: 3}),
+  defp handle_output(%Result{status: 3}),
   do: {:error, "Unknown option"}
 
-  defp handle_output(%{status: 4}),
+  defp handle_output(%Result{status: 4}),
   do: {:error, "Failed to parse the stylesheet"}
 
-  defp handle_output(%{status: 5}),
+  defp handle_output(%Result{status: 5}),
   do: {:error, "Error in the stylesheet"}
 
-  defp handle_output(%{status: 6}),
+  defp handle_output(%Result{status: 6}),
   do: {:error, "Error in one of the documents"}
 
-  defp handle_output(%{status: 7}),
+  defp handle_output(%Result{status: 7}),
   do: {:error, "Unsupported xsl:output method"}
 
-  defp handle_output(%{status: 8}),
+  defp handle_output(%Result{status: 8}),
   do: {:error, "String parameter contains both quote and double-quotes"}
 
-  defp handle_output(%{status: 9}),
+  defp handle_output(%Result{status: 9}),
   do: {:error, "Internal Processing error"}
 
-  defp handle_output(%{status: 10}),
+  defp handle_output(%Result{status: 10}),
   do: {:error, "Processing was stopped by a terminating message"}
 
-  defp handle_output(%{status: 11}),
+  defp handle_output(%Result{status: 11}),
   do: {:error, "Could not write the result to the output file"}
 
 end
