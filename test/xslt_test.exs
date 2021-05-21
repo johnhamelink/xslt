b1fc1046667cd9300dcec7725983ba4a1bac9f19
Martin Berger <martin.berger@cpg.de>
2021-05-21 15:27:17 +0200

    Add param --xml-as-string to additionally transform a xml-string besides xml-file

--- test/xslt_test.exs
+++ test/xslt_test.exs
@@ -7,10 +7,17 @@ defmodule XsltTest do
     template = Path.expand("./test/fixtures/product/xslt_template.xml")
     assert {:ok, _xml} = Xslt.transform(template, xml)
   end
+
   test "Happy path using params" do
     xml = Path.expand("./test/fixtures/product/mws_doc_example.xml")
     template = Path.expand("./test/fixtures/product/xslt_template.xml")
     assert {:ok, _xml} = Xslt.transform(template, xml, "--stringparam param value")
+  end
+
+  test "Happy path using param --xml-as-string" do
+    xml = File.read!(Path.expand("./test/fixtures/product/simple_example.xml"))
+    template = Path.expand("./test/fixtures/product/xslt_template.xml")
+    assert {:ok, _xml} = Xslt.transform(template, xml, "--xml-as-string")
   end
 
   test "Unhappy path" do
@@ -22,24 +29,29 @@ defmodule XsltTest do
     template = Path.expand("./test/fixtures/product/doesntexist.xml")
     assert {:error, _} = Xslt.transform(template, xml)
   end
+
   test "Guards should return xml processed" do
-    xml =  Path.expand("./test/fixtures/product/mws_doc_example.xml")
+    xml = Path.expand("./test/fixtures/product/mws_doc_example.xml")
     template = Path.expand("./test/fixtures/product/xslt_template.xml")
     assert {:ok, _xml} = Xslt.transform(template, xml, nil)
     assert {:ok, _xml} = Xslt.transform(template, xml, :anything)
     assert {:ok, _xml} = Xslt.transform(template, xml, [])
   end
+
   test "Guards should return same xmls" do
-    xml =  Path.expand("./test/fixtures/product/mws_doc_example.xml")
+    xml = Path.expand("./test/fixtures/product/mws_doc_example.xml")
     {:ok, convert} = Xslt.transform(:anything, xml, "--stringparam param value")
     {:ok, fixture} = File.read(xml)
     assert fixture == convert
   end
+
   test "Guards should return a non enoent error" do
-    xml =  Path.expand("./test/fixtures/product/doesntexist.xml")
+    xml = Path.expand("./test/fixtures/product/doesntexist.xml")
     assert {:error, :enoent} = Xslt.transform(:anything, xml, "--stringparam param value")
   end
+
   test "Guards should return a bad_arguments error" do
-    assert {:error, :bad_arguments} = Xslt.transform(:anything, :anything, "--stringparam param value")
+    assert {:error, :bad_arguments} =
+             Xslt.transform(:anything, :anything, "--stringparam param value")
   end
 end
